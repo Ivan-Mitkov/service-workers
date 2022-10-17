9052183c7cb824e65ad75333ccf94c6d66f70e70
Ivan Mitkov <ivan.mitkov@gmail.com>
2022-10-17 17:50:32 +0300

    add content not related to sw

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,7 +1,10 @@
 console.log("APP.js", navigator);
 if ("serviceWorker" in navigator) {
   console.log("HERE");
-  navigator.serviceWorker.register("../sw.js").then(function () {
-    console.log("Service worker registered!");
-  });
+  navigator.serviceWorker
+    .register("../sw.js")
+    .then(function () {
+      console.log("Service worker registered!");
+    })
+    .catch((err) => console.log(err));
 }
--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,15 @@
+// triggered by web browser
 self.addEventListener("install", function (e) {
   console.log("[servisce worker install]", e);
 });
-
+// triggered by web browser
 self.addEventListener("activate", function (e) {
   console.log("[servisce worker activate]", e);
   return self.clients.claim();
+});
+
+// triggered by web app
+self.addEventListener("fetch", (e) => {
+  console.log("[fetching]", e);
+  e.respondWith(fetch(e.request));
 });
